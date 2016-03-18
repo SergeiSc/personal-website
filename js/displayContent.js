56513c2f7abdd3c1f0468895ac98b2c2cbc812e8
SergeiSc <serjoga911@live.ru>
2016-03-18 16:13:26 +0000

    unfinished functionality for multiple backgrounds

--- js/displayContent.js
+++ js/displayContent.js
@@ -1,18 +1,35 @@
 $(window).on('hashchange', function() {
-  
-	switch(window.location.hash){	
-		case '#home' :
-			$('#home-panel').addClass('active');
-  			$('#home-panel').siblings().removeClass('active');
-			break;
-		case '#about' :
-			$('#about-panel').addClass('active');
-  			$('#about-panel').siblings().removeClass('active');
-			break;
-		case '#contact' :
-    		$('#contact-panel').addClass('active');
-  			$('#contact-panel').siblings().removeClass('active');
-			break;
-	}
-  
+	switchPanels();
 });
+
+$(window).load(function() {
+	switchPanels();
+});
+
+function switchPanels(){
+	
+	switch(window.location.hash){	
+	case '#home' :
+		$('body').removeClass();
+		$('body').addClass('bg-img1 animate-opacity');
+
+		$('#home-panel').addClass('active');
+  		$('#home-panel').siblings().removeClass('active');
+		break;
+	case '#about' :
+		$('body').removeClass();
+		$('body').addClass('bg-img2 animate-opacity');
+
+		$('#about-panel').addClass('active');
+  		$('#about-panel').siblings().removeClass('active');
+		break;
+	case '#contact' :
+		$('body').removeClass();
+		$('body').addClass('bg-img3 animate-opacity');
+
+		$('#contact-panel').addClass('active');
+  		$('#contact-panel').siblings().removeClass('active');
+		break;
+	}
+	
+}
