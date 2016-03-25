d08add412304bb369dd993d91f26a15f610b3ec8
SergeiSc <serjoga911@live.ru>
2016-03-25 10:46:34 +0000

    Updated background and main panels
    Updated background js and css
    Updated main panels in css

--- js/displayContent.js
+++ js/displayContent.js
@@ -10,22 +10,28 @@ function switchPanels(){
 	
 	switch(window.location.hash){	
 	case '#home' :
+		/*
 		$('body').removeClass();
-		$('body').addClass('bg-img1 animate-opacity');
+		$('body').addClass('bg-img1 cover crossfadeBg');
+		*/
 
 		$('#home-panel').addClass('active');
   		$('#home-panel').siblings().removeClass('active');
 		break;
 	case '#about' :
+		/*
 		$('body').removeClass();
-		$('body').addClass('bg-img2 animate-opacity');
+		$('body').addClass('bg-img2 cover crossfadeBg');
+		*/
 
 		$('#about-panel').addClass('active');
   		$('#about-panel').siblings().removeClass('active');
 		break;
 	case '#contact' :
+		/*
 		$('body').removeClass();
-		$('body').addClass('bg-img3 animate-opacity');
+		$('body').addClass('bg-img3 cover crossfadeBg');
+		*/
 
 		$('#contact-panel').addClass('active');
   		$('#contact-panel').siblings().removeClass('active');
